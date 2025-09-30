8e65732b11d2b95aa14c731a4eac17e2107890b0
moumitakarmakar123 <moumitakarmakar.work@gmail.com>
2025-09-30 22:39:35 +0530

    Update Transaction.java
    Make Transaction

--- expense_tracker/src/Transaction.java
+++ expense_tracker/src/Transaction.java
@@ -2,13 +2,13 @@
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
-import java.util.List;
 
 public class Transaction {
 
-  private double amount;
-  private String category;
-  private String timestamp;
+  private final double amount;
+  private final String category;
+  private final String timestamp;
+
 
   public Transaction(double amount, String category) {
     this.amount = amount;
@@ -16,22 +16,16 @@ public class Transaction {
     this.timestamp = generateTimestamp();
   }
 
+ 
   public double getAmount() {
     return amount;
-  }
-
-  public void setAmount(double amount) {
-    this.amount = amount;
   }
 
   public String getCategory() {
     return category;
   }
-
-  public void setCategory(String category) {
-    this.category = category; 
-  }
   
+ 
   public String getTimestamp() {
     return timestamp;
   }
